6eb7ea1665fdcce5969ae31937c88c069c0bea40
gabman15 <32818424+gabman15@users.noreply.github.com>
2019-07-16 16:25:30 -0400

    I HAVE TO PRESS MY MOUSE

--- Bot.js
+++ Bot.js
@@ -16,7 +16,7 @@ client.on("message",(message) => {
 	const command = args.shift().toLowerCase();
 
 	if (command === 'kill' && message.member.id == auth.ownerID) {
-		message.channel.send("I'm Sorry Dave, I'm Afraid I Can't Do That."{tts: true});
+		message.channel.send("I'm Sorry Dave, I'm Afraid I Can't Do That.",{tts: true});
 	}
 	else if (message.member.id == auth.targetID) {
 		message.channel.send("No.");
