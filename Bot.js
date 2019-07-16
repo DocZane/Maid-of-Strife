62f7f25f29570b7ec6b59658b0b8dd1583ac5788
gabman15 <32818424+gabman15@users.noreply.github.com>
2019-07-16 15:29:13 -0400

    add 1 semicolon wryyyyy

--- Bot.js
+++ Bot.js
@@ -10,7 +10,7 @@ client.on("message",(message) => {
 	if (!message.content.startsWith(auth.prefix) || message.author.bot)
 		return;
 	if (message.content.startsWith(auth.prefix+"kill")&& message.member.id == auth.ownerID) {
-		message.channel.send("I'm Sorry Dave, I'm Afraid I Can't Do That.")
+		message.channel.send("I'm Sorry Dave, I'm Afraid I Can't Do That.");
 	}
 	else if(message.content.startsWith(auth.prefix+"ping")) {
 		message.channel.send("Pong!");
