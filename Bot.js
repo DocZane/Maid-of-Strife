f6e574a9e42e1ef59df69496811e4435fc78ac2e
gabman15 <32818424+gabman15@users.noreply.github.com>
2019-07-16 15:25:52 -0400

    fix indent

--- Bot.js
+++ Bot.js
@@ -7,34 +7,41 @@ client.on("ready",() => {
 });
 
 client.on("message",(message) => {
-	if(!message.content.startsWith(auth.prefix) || message.author.bot) return;
-	if(message.content.startsWith(auth.prefix+"kill")&& message.member.id == auth.ownerID){
+	if (!message.content.startsWith(auth.prefix) || message.author.bot)
+		return;
+	if (message.content.startsWith(auth.prefix+"kill")&& message.member.id == auth.ownerID) {
 		message.channel.send("I can't let you do that, Dave.")
-	} else 
-	if(message.content.startsWith(auth.prefix+"ping")){
+	}
+	else if (message.content.startsWith(auth.prefix+"ping")) {
 		message.channel.send("Pong!");
-	} else
-	if(message.content.startsWith(auth.prefix+"pong")){
+	}
+	else if (message.content.startsWith(auth.prefix+"pong")) {
 		message.channel.send("Ping!")
-	} else
-	if(message.content.startsWith(auth.prefix+"Am I a Helper")){
-		if(message.member.roles.get('596787797998698522')){
+	}
+	else if(message.content.startsWith(auth.prefix+"Am I a Helper")) {
+		if (message.member.roles.get('596787797998698522')) {
 			message.channel.send("Yes, You are!")
-		} else {
+		}
+		else {
 			message.channel.send("No, You aren't!")
-	}} else
-	if(message.content.startsWith(auth.prefix+"Am I a DM")){
-		if(message.member.roles.get('596485222443515924')){
+		}
+	}
+	else if(message.content.startsWith(auth.prefix+"Am I a DM")) {
+		if(message.member.roles.get('596485222443515924')) {
 			message.channel.send("Yes, You are!")
-		} else {
+		}
+		else {
 			message.channel.send("No, You aren't!")
-	}} else
-	if(message.content.startsWith(auth.prefix+"Am I a Player")){
-		if(message.member.roles.get('596558352327114754')){
+		}
+	}
+	else if(message.content.startsWith(auth.prefix+"Am I a Player")) {
+		if(message.member.roles.get('596558352327114754')) {
 			message.channel.send("Yes, You are!")
-		} else {
+		}
+		else {
 			message.channel.send("No, You aren't!")
-	}}
+		}
+	}
 });
 
 client.login(auth.token)
