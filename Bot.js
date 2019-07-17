7676858130c692476dd235aaa479ff30e3f329a4
DocZane <52976313+DocZane@users.noreply.github.com>
2019-07-16 20:58:13 -0400

    Enmap and updates

--- Bot.js
+++ Bot.js
@@ -4,22 +4,48 @@ const auth = require("../Maid of Strife Json dump/auth.json");
 const fs = require("fs");
 const Enmap = require("enmap");
 
+clent.auth = auth;
 
 client.on("ready",() => {
 	console.log("I am Ready!");
 });
 
+// This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
+    // If the file is not a JS file, ignore it (thanks, Apple)
+    if (!file.endsWith(".js")) return;
+    // Load the event file itself
     const event = require(`./events/${file}`);
+    // Get just the event name from the file name
     let eventName = file.split(".")[0];
+    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
+    // without going into too many details, this means each event will be called with the client argument,
+    // followed by its "normal" arguments, like message, member, etc etc.
+    // This line is awesome by the way. Just sayin'.
     client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/${file}`)];
   });
 });
 
+client.commands = new Enmap();
 
-client.on("message", async message => {
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    // Load the command file itself
+    let props = require(`./commands/${file}`);
+    // Get just the command name from the file name
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    // Here we simply store the whole thing in the command Enmap. We're not running it right now.
+    client.commands.set(commandName, props);
+  });
+});
+
+/*client.on("message", async message => {
 	if (!message.content.startsWith(auth.prefix) || message.author.bot)
 		return;
 
@@ -29,9 +55,9 @@ client.on("message", async message => {
 	if (command === 'kill' && message.member.id == auth.ownerID) {
 		message.channel.send("I'm Sorry Dave, I'm Afraid I Can't Do That.",{tts: true});
 	}
-	/*else if (message.member.id == auth.targetID) {
+	else if (message.member.id == auth.targetID) {
 		message.channel.send("No.");
-	}*/
+	}
 	else if (command === 'ping') {
 		message.channel.send("Pong!");
 	}
@@ -63,11 +89,11 @@ client.on("message", async message => {
 				//.catch (error) { console.error('dIdNt WoRk FuCkEr');
 
 
-		/*const one = client.emojis.find(emoji => emoji.name === 'one')
-		message.react(one.id);*/
-		/*message.react((client.emojis.find(emoji => emoji.name === 'two')).id);
-		message.react((client.emojis.find(emoji => emoji.name === 'three')).id);*/
-	}
+		const one = client.emojis.find(emoji => emoji.name === 'one')
+		message.react(one.id);
+		message.react((client.emojis.find(emoji => emoji.name === 'two')).id);
+		message.react((client.emojis.find(emoji => emoji.name === 'three')).id);
+	}*/
 
 
 	/*else if (command === 'helper') {
@@ -94,6 +120,5 @@ client.on("message", async message => {
 			message.channel.send("No, You aren't a player!");
 		}
 	}*/
-});
 
-client.login(auth.token)
+client.login(auth.token);
