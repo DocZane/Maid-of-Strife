5859363645cbcc7cdbb260085122441246cc2b0c
DocZane <52976313+DocZane@users.noreply.github.com>
2019-07-17 19:50:13 -0400

    added suppression function, and ping function to >emote

--- commands/emote.js
+++ commands/emote.js
@@ -1,10 +1,15 @@
 exports.run = (client, message, args) => {
 var emote = args[0];
+var sup = args[1];
 var emoteName = client.emojis.find(emoji => emoji.name === emote);
 if (emoteName === null){
   message.channel.send("I'm sorry, I don't know that emote!")
 } else {
   message.delete();
-  message.channel.send(`${emoteName}`);
+    if(sup === 'sup'){
+    message.channel.send(`${emoteName}`);
+  } else {
+    message.reply(`${emoteName}`);
+  }
   }
 }
