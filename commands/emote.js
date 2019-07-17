8c5a678c986bc2a9a8405c65f53650510a94d03b
DocZane <52976313+DocZane@users.noreply.github.com>
2019-07-17 19:38:04 -0400

    added a catch if the emote isnt recognised, and it deletes the message of the sender.

--- commands/emote.js
+++ commands/emote.js
@@ -1,5 +1,10 @@
 exports.run = (client, message, args) => {
-var emote = args[0]
+var emote = args[0];
 var emoteName = client.emojis.find(emoji => emoji.name === emote);
-message.channel.send(`${emoteName}`)
+if (emoteName === null){
+  message.channel.send("I'm sorry, I don't know that emote!")
+} else {
+  message.delete();
+  message.channel.send(`${emoteName}`);
+  }
 }
