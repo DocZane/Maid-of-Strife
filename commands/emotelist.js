7a673aea67108e870564542e6428174edd2c1b67
DocZane <52976313+DocZane@users.noreply.github.com>
2019-10-08 00:07:31 -0400

    fixed emotelist, made it so only I can use it, its a nightmare.

--- commands/emotelist.js
+++ commands/emotelist.js
@@ -1,11 +1,22 @@
-
-//Currently Broken, too many emotes.
+const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
-  /*
-  const emojiList = client.emojis.map((e, x) => (e) + ' | ' +e.name).join('\n');
-//message.delete();
-  message.channel.send(emojiList)
-    .then(foo => {setTimeout(function(){foo.delete()},10000)});
-  message.delete();*/
-}
+  if (message.member.id == client.auth.ownerID){
+    var elist = client.emojis.map((e, x) => (e) + ' | ' +e.name);
+    var lennum = elist.length
+    var remaining = 2000;
+    let page = 1;
+    let pages =[];
+      for (i=0; i<lennum+1; i++){
+        remaining = remaining-elist.length
+        console.log(remaining);
+          if (remaining <= 0){
+        remaining = 2000+remaining;
+        message.channel.send(pages);
+        pages = [];
+        pages.push(elist[i]);
+            } else {
+              pages.push(elist[i]);
+          }
+        };
+    };
